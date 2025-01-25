ae499afb4765bbe1ef3c0880ffc68d791a4df8c5
Solomon S. <solomon.sefiw@berhanbanksc.com>
2025-01-24 21:48:34 -0800

    first commit

--- src/services/emptySplitApi.ts
+++ src/services/emptySplitApi.ts
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const emptySplitApi = createApi({
-  reducerPath: "HCMSApi",
+  reducerPath: "AIDAApi",
   baseQuery: fetchBaseQuery({
     baseUrl: "http://localhost:5023", // Base API URL
     prepareHeaders: (headers) => {
